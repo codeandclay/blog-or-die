46134ae0e15e5aeafbdca4be70bd046daa46a177
Oliver <oliver@therewillbeanimals.com>
2018-05-23 14:43:58 +0100

    Extract all to class methods

--- blog-or-die.php
+++ blog-or-die.php
@@ -12,35 +12,40 @@ Version: 1.0
 Author URI: www.codeandclay.com
 */
 
-function cc_prevent_page_load() {
-	if ( cc_is_last_post_older_than( cc_time_limit_in_seconds() ) ) {
-		cc_death_notice();
+class CCBlogOrDie {
+	public static function init() {
+		add_action( 'template_redirect', array( __CLASS__, 'prevent_page_load' ) );
+	}
+
+	public static function prevent_page_load() {
+		if ( self::is_last_post_older_than( self::time_limit_in_seconds() ) ) {
+			self::death_notice();
+		}
+	}
+
+	private static function is_last_post_older_than( $seconds ) {
+		return self::time_of_latest_post_in_seconds() > $seconds;
+	}
+
+	private static function time_limit_in_seconds() {
+		return 10;
+	}
+
+	private static function time_of_latest_post_in_seconds() {
+		// It is not gauranteed that the first item returned by get_posts() is the
+		// latest post. So here I compare the timestamps of all posts.
+		$times = array_map(
+			function( $post ) {
+				return $post->post_date;
+			}, get_posts()
+		);
+		sort( $times );
+		return end( $times );
+	}
+
+	private static function death_notice() {
+		die( '<h1>Your blog is dead</h1>' );
 	}
 }
 
-function cc_is_last_post_older_than( $seconds ) {
-	return cc_time_of_latest_post_in_seconds() > $seconds;
-}
-
-function cc_time_limit_in_seconds() {
-	return 10;
-}
-
-function cc_time_of_latest_post_in_seconds() {
-	// It is not gauranteed that the first item returned by get_posts() is the
-	// latest post. So here I compare the timestamps of all posts.
-	$times = array_map(
-		function( $post ) {
-			return $post->post_date;
-		}, get_posts()
-	);
-	sort( $times );
-	return end( $times );
-}
-
-function cc_death_notice() {
-	// wp_die('Your blog is dead.');
-	die( '<h1>Your blog is dead</h1>' );
-}
-
-add_action( 'template_redirect', 'cc_prevent_page_load' );
+CCBlogOrDie::init();
