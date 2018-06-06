fcfc6fab48e81c3a5932e5ec89c5bdc66f074a05
Oliver <oliver@therewillbeanimals.com>
2018-06-06 10:12:25 +0100

    Simplify BlogOrDieFuzzyTimeAgo
    I've simplified `time_periods_passed()` and made a few knock on changes.
    
    The returned list now contains strings instead of numbers and there is no need
    for the method that returns the age of the timestamp.

--- class-blogordiefuzzytimeago.php
+++ class-blogordiefuzzytimeago.php
@@ -14,38 +14,28 @@ class BlogOrDieFuzzyTimeAgo {
 		$longest_period = array_slice( $periods, 0, 1 );
 
 		// I prefer: 'a day' over '1 days'
-		if ( reset( $longest_period ) === 1 ) {
+		if ( reset( $longest_period ) === '1' ) {
 			$longest_period = [ substr( key( $longest_period ), 0, -1 ) => 'a' ];
 		}
 
 		return 'over ' . reset( $longest_period ) . ' ' . key( $longest_period ) . ' ago';
 	}
 
-	private function age_of_reference_timestamp() {
-		return current_time( 'timestamp' ) - $this->reference_timestamp;
-	}
-
 	private function periods() {
 		return [
-			'seconds' => 1,
-			'minutes' => 60,
-			'hours'   => 60,
-			'days'    => 24,
-			'years'   => 365,
+			'seconds',
+			'minutes',
+			'hours',
+			'days',
+			'years',
 		];
 	}
 
 	private function time_periods_passed() {
-		// TODO: Find a more elegant way to do this
-		$periods_elapsed = array_reduce(
-			array_slice( $this->periods(), 1 ), function( $acc, $period ) {
-				return array_merge(
-					$acc, [
-						intdiv( end( $acc ), $period ),
-					]
-				);
-			}, [ $this->age_of_reference_timestamp() ]
-		);
-		return array_combine( array_keys( $this->periods() ), $periods_elapsed );
+		$now  = new DateTime();
+		$then = new DateTime( "@$this->reference_timestamp" );
+
+		$difference = $now->diff( $then )->format( '%s,%i,%h,%a,%y' );
+		return array_combine( $this->periods(), explode( ',', $difference ) );
 	}
 }
