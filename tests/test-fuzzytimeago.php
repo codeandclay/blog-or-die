6167687bd422327e895e59e56f9c00d4ef5dacb0
Oliver <oliver@therewillbeanimals.com>
2018-06-06 22:01:18 +0100

    fixup! Provide way to get approximate age of timestamp

--- tests/test-fuzzytimeago.php
+++ tests/test-fuzzytimeago.php
@@ -11,6 +11,6 @@ class FuzzyTimeAgoTest extends WP_UnitTestCase {
 	public function test_over_one_day() {
 		// initialize object with a day and one second
 		$subject = new BlogOrDieFuzzyTimeAgo( current_time( 'timestamp' ) - 86401 );
-		$this->assertTrue( $subject->description() === 'over a day ago' );
+		$this->assertSame( $subject->description(), 'over a day ago' );
 	}
 }
